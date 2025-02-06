e82a1924c768350057bd4ec154b70e4d1e83b3ac
OllaWilk <aleksandra.wilk.berserksoft@gmail.com>
2025-02-06 11:15:09 +0100

    Added state management to the Player component for name editing functionality

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Player.css';
 
-export const Player = ({ playerName, symbol }) => (
-  <li>
-    <span className='player'>
-      <span className='player-name'>{playerName}</span>
-      <span className='player-symbol'>{symbol}</span>
-    </span>
-    <button>Edit</button>
-  </li>
-);
+export const Player = ({ playerName, symbol }) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  let name = <span className='player-name'>{playerName}</span>;
+
+  if (isEditing) {
+    name = <input type='text' required />;
+  }
+
+  return (
+    <li>
+      <span className='player'>
+        {name}
+        <span className='player-symbol'>{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>Edit</button>
+    </li>
+  );
+};
