2f42948ce93d1dbd4a4cf067e913ce4207a30c89
OllaWilk <aleksandra.wilk.berserksoft@gmail.com>
2025-02-07 12:47:02 +0100

    Added Log component to display the history of player moves. Each turn is recorded and rendered as a list item, showing which player selected which square. Ensured proper key assignment for efficient re-rendering

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ export function App() {
         </ol>
         <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
-      <Log />
+      <Log turns={gameTurns} />
     </main>
   );
 }
