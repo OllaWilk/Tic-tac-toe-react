031028b3f1f5a083dde429af02aaa72ccd873c32
OllaWilk <aleksandra.wilk.berserksoft@gmail.com>
2025-02-07 12:47:02 +0100

    Added Log component to display the history of player moves. Each turn is recorded and rendered as a list item, showing which player selected which square. Ensured proper key assignment for efficient re-rendering

--- src/components/Log/Log.jsx
+++ src/components/Log/Log.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import './Log.css';
 
-export const Log = () => {
-  return <ol id='log'></ol>;
+export const Log = ({ turns }) => {
+  return (
+    <ol id='log'>
+      {turns.map((turn) => (
+        <li key={`${turn.square.row}-${turn.square.col} `}>
+          {turn.player} selected {turn.square.row}, {turn.square.col}
+        </li>
+      ))}
+    </ol>
+  );
 };
